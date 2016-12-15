7b0ec96d0aa779569bc465911037e06cc0800bf2
jiangchen <1760247442@qq.com>
2016-12-15 21:48:01 +0800

    first push

---
+++ src/main/java/Algs4/Test.java
@@ -0,0 +1,12 @@
+package Algs4;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by jiangchen on 2016/12/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        StdOut.println("Hello,World!");
+    }
+}
