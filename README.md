cb1b82f365315aa70fdc3a1759b692d4ee5b027b
Scott Shipp <scott.shipp@openmarket.com>
2018-07-12 08:14:07 -0700

    Add and test binary search tree zigzag traversals

--- README.md
+++ README.md
@@ -4,13 +4,18 @@ This is a fork of kevin-wayne/algs4, the code from the popular textbook _Algorit
 
 ## How is this different?
 
-My fork is mostly the same but I am updating the repository using modern Java standards.
+My fork is mostly the same but uses modern Java tools and standards.
 
-The codebase uses SLF4J for logging JUnit 5 to unit test the code I've touched. Unit tests have been added in the idiomatic location (src/test/java).
+For example, I have added:
 
-Data files are provided separately as a zip download on the [online book site](https://algs4.cs.princeton.edu/home/) but I am copying the ones I'm using into the idiomatic location (src/main/resources).
+* SLF4J for logging 
+* JUnit 5 to unit test the code I've touched
+ 
+Unit tests have also been added in a new location (src/test/java) according to popular standard.
 
-I have also added some new packages, found under the namespace _com.scottshipp.code_. Included in this, I have been working on the *exercises* given in the [online book site](https://algs4.cs.princeton.edu/home/). In some cases, I am also practicing re-implementing fundamental algorithms on paper, then manually copying my paper-written code into the appropriate package where I unit test it. 
+Another difference is that while data files are provided separately as a zip download on the [online book site](https://algs4.cs.princeton.edu/home/), I have directly included any that I've used into the idiomatic location (src/main/resources).
+
+I have also added some new packages, found under the namespace _com.scottshipp.code_. Included in this, I have been working on providing answers to selected *exercises* from the [online book site](https://algs4.cs.princeton.edu/home/). In some cases, I am also practicing re-implementing fundamental algorithms on paper, then manually copying my paper-written code into the appropriate package where I unit test it. 
 
 I'm doing that to re-learn algorithms and keep sharp.
 
@@ -22,3 +27,5 @@ every serious programmer
 needs to know about
 algorithms and data structures
 ```
+
+You can learn more about me, Java, and software engineering in general at [code.scottshipp.com](http://code.scottshipp.com).
---
+++ src/test/java/com/scottshipp/code/algs4/bst/BinarySearchTreeTest.java
@@ -0,0 +1,110 @@
+package com.scottshipp.code.algs4.bst;
+
+import edu.princeton.cs.algs4.In;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinarySearchTreeTest {
+    private static Logger logger = Logger.getLogger(BinarySearchTreeTest.class.toString());
+
+    private BinarySearchTree<String, Integer> st;
+    @BeforeEach
+    void setup() {
+        // populate BST with tinyST
+
+        st = new BinarySearchTree<>();
+        In in = new In("anotherTinyST.txt");
+        for (int i = 0; !in.isEmpty(); i++) {
+            String key = in.readString();
+            st.put(key, i);
+        }
+
+    }
+
+    void alternateSetup() {
+        st = new BinarySearchTree<>();
+        In in = new In("tinyST.txt");
+        for (int i = 0; !in.isEmpty(); i++) {
+            String key = in.readString();
+            st.put(key, i);
+        }
+    }
+
+    @Test
+    public void testInitialTreePopulation() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.levelOrder()) {
+            sb.append(s + " " + st.get(s) + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("A 7 T 1 R 2 X 6 E 11 M 8 L 10 P 9 ", sb.toString());
+    }
+
+    @Test
+    public void testLevelOrderTraversal() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.levelOrder()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("A T R X E M L P ", sb.toString());
+    }
+
+    @Test
+    public void testLevelOrderTraversal2() {
+        alternateSetup();
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.levelOrder()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("S E X A R C H M L P ", sb.toString());
+    }
+
+    @Test
+    public void testZigZagTraversal() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.zigZagOrder()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("A T R X E M P L ", sb.toString());
+    }
+
+    @Test
+    public void testZigZagTraversal2() {
+        alternateSetup();
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.zigZagOrder()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("S X E A R H C M P L ", sb.toString());
+    }
+
+    @Test
+    public void testReverseZigZagTraversal() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.reverseZigZagOrder2()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("P L M E R X T A ", sb.toString());
+    }
+
+    @Test
+    public void testAnotherReverseZigZagTraversal() {
+        alternateSetup();
+        StringBuilder sb = new StringBuilder();
+        for (String s : st.reverseZigZagOrder()) {
+            sb.append(s + " ");
+        }
+        logger.info(sb.toString());
+        assertEquals("L P M C H R A E X S ", sb.toString());
+    }
+}
+
