c77772be92039294afdad39ec402cee99bfeca69
Scott Shipp <gitlab@scottshipp.com>
2018-12-29 20:21:28 -0800

    Fix merge sort, removing unused code, and ordering by call

--- src/main/java/edu/princeton/cs/algs4/Merge.java
+++ src/main/java/edu/princeton/cs/algs4/Merge.java
@@ -33,6 +33,26 @@ public class Merge {
   // This class should not be instantiated.
   private Merge() {}
 
+  /**
+   * Rearranges the array in ascending order, using the natural order.
+   *
+   * @param a the array to be sorted
+   */
+  public static void sort(Comparable[] a) {
+    Comparable[] aux = new Comparable[a.length];
+    sort(a, aux, 0, a.length - 1);
+    assert isSorted(a);
+  }
+
+  // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
+  private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+    if (hi <= lo) return;
+    int mid = lo + (hi - lo) / 2;
+    sort(a, aux, lo, mid);
+    sort(a, aux, mid + 1, hi);
+    merge(a, aux, lo, mid, hi);
+  }
+
   // stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
   private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
     // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
@@ -57,25 +77,7 @@ public class Merge {
     assert isSorted(a, lo, hi);
   }
 
-  // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-  private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
-    if (hi <= lo) return;
-    int mid = lo + (hi - lo) / 2;
-    sort(a, aux, lo, mid);
-    sort(a, aux, mid + 1, hi);
-    merge(a, aux, lo, mid, hi);
-  }
 
-  /**
-   * Rearranges the array in ascending order, using the natural order.
-   *
-   * @param a the array to be sorted
-   */
-  public static void sort(Comparable[] a) {
-    Comparable[] aux = new Comparable[a.length];
-    sort(a, aux, 0, a.length - 1);
-    assert isSorted(a);
-  }
 
   /**
    * ************************************************************************* Helper sorting
@@ -99,54 +101,6 @@ public class Merge {
   private static boolean isSorted(Comparable[] a, int lo, int hi) {
     for (int i = lo + 1; i <= hi; i++) if (less(a[i], a[i - 1])) return false;
     return true;
-  }
-
-  /**
-   * ************************************************************************* Index mergesort.
-   * *************************************************************************
-   */
-  // stably merge a[lo .. mid] with a[mid+1 .. hi] using aux[lo .. hi]
-  private static void merge(Comparable[] a, int[] index, int[] aux, int lo, int mid, int hi) {
-
-    // copy to aux[]
-    for (int k = lo; k <= hi; k++) {
-      aux[k] = index[k];
-    }
-
-    // merge back to a[]
-    int i = lo, j = mid + 1;
-    for (int k = lo; k <= hi; k++) {
-      if (i > mid) index[k] = aux[j++];
-      else if (j > hi) index[k] = aux[i++];
-      else if (less(a[aux[j]], a[aux[i]])) index[k] = aux[j++];
-      else index[k] = aux[i++];
-    }
-  }
-
-  /**
-   * Returns a permutation that gives the elements in the array in ascending order.
-   *
-   * @param a the array
-   * @return a permutation {@code p[]} such that {@code a[p[0]]}, {@code a[p[1]]}, ..., {@code
-   *     a[p[N-1]]} are in ascending order
-   */
-  public static int[] indexSort(Comparable[] a) {
-    int n = a.length;
-    int[] index = new int[n];
-    for (int i = 0; i < n; i++) index[i] = i;
-
-    int[] aux = new int[n];
-    sort(a, index, aux, 0, n - 1);
-    return index;
-  }
-
-  // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-  private static void sort(Comparable[] a, int[] index, int[] aux, int lo, int hi) {
-    if (hi <= lo) return;
-    int mid = lo + (hi - lo) / 2;
-    sort(a, index, aux, lo, mid);
-    sort(a, index, aux, mid + 1, hi);
-    merge(a, index, aux, lo, mid, hi);
   }
 
   // print array to standard output
