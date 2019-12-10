6dcf6c6f0a4c1ea3c368a263ddefe97fbde87666
Jai Shankar K <tojaish@gmail.com>
2019-12-10 12:43:15 -0500

    Test Code Guru

--- src/main/java/edu/princeton/cs/algs4/AcyclicLP.java
+++ src/main/java/edu/princeton/cs/algs4/AcyclicLP.java
@@ -76,6 +76,7 @@ public class AcyclicLP {
     // relax edge e, but update if you find a *longer* path
     private void relax(DirectedEdge e) {
         int v = e.from(), w = e.to();
+        
         if (distTo[w] < distTo[v] + e.weight()) {
             distTo[w] = distTo[v] + e.weight();
             edgeTo[w] = e;
