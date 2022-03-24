727ebf04cb8eb9112aab090598af0fbf7d184f37
Ayush Verma <ay322266@dal.ca>
2022-03-24 13:11:06 -0300

    Removed method from the original source

--- src/main/java/edu/princeton/cs/algs4/Complex.java
+++ src/main/java/edu/princeton/cs/algs4/Complex.java
@@ -210,33 +210,6 @@ public class Complex {
     public Complex exp() {
         return new Complex(Math.exp(re) * Math.cos(im), Math.exp(re) * Math.sin(im));
     }
-
-    /**
-     * Returns the complex sine of this complex number.
-     *
-     * @return the complex sine of this complex number
-     */
-    public Complex sin() {
-        return new Complex(Math.sin(re) * Math.cosh(im), Math.cos(re) * Math.sinh(im));
-    }
-
-    /**
-     * Returns the complex cosine of this complex number.
-     *
-     * @return the complex cosine of this complex number
-     */
-    public Complex cos() {
-        return new Complex(Math.cos(re) * Math.cosh(im), -Math.sin(re) * Math.sinh(im));
-    }
-
-    /**
-     * Returns the complex tangent of this complex number.
-     *
-     * @return the complex tangent of this complex number
-     */
-    public Complex tan() {
-        return sin().divides(cos());
-    }
     
 
     /**
